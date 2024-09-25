3ddafc9674d37aaa03a54f1abbd0d7c1fdd46cb0
laerling <laerling@posteo.de>
2024-09-25 20:22:27 +0200

    phash.py: 1x1 to 4x4 inner square

--- phash.py
+++ phash.py
@@ -13,25 +13,35 @@ for f in sys.argv[1:]:
 
     # load image, silently ignoring non-image files
     try:
-        i = Image.open(f)
+        img = Image.open(f)
     except UnidentifiedImageError:
         continue
 
+    # TODO: Convert alpha to white instead of black, since that's how I want alpha to be displayed
+
+    # TODO: Increase contrast to have a unified dynamic range (or whatever it's
+    #       called) across all images and to distinguish between different
+    #       details in images with low contrast
+
     # convert to grayscale, since we're not interested in color shifts
-    i = i.convert('L')
+    img = img.convert('L')
 
-    # resize
-    # TODO: Which filter gives best result?
-    #       BOX, BILINEA, HAMMING, BICUBIC, LANCZOS
-    resampling_filter = Image.Resampling.LANCZOS
-    i = i.resize((TO_DIM,TO_DIM), resampling_filter)
+    # inner square (i. e. using n**2 of (n+2)**2 pixels) with n going from 1 to TO_DIM
+    for n in range(1, TO_DIM+1):
 
-    # FIXME make bit array and print as bytes (e. g. 4*256/16 would be 4 bytes,
-    # but 4*256/8 would be 5)
-    px = i.load()
-    for i in range(TO_DIM):
-        for j in range(TO_DIM):
-            print(f"{px[i,j]//16:1x}",end='')
+        # resize
+        # TODO: Which filter gives best result?
+        #       BOX, BILINEA, HAMMING, BICUBIC, LANCZOS
+        resampling_filter = Image.Resampling.LANCZOS
+        img_ = img.resize((n+2,n+2), resampling_filter)
+
+        # FIXME make bit array and print as bytes (e. g. 4*256/16 would be 4 bytes,
+        # but 4*256/8 would be 5)
+        px = img_.load()
+        for i in range(1, n+1):
+            for j in range(1, n+1):
+                print(f"{px[i,j]//16:1x}", end='')
+        print(end=' ')
 
     # print filename
     print(f" {f}")
