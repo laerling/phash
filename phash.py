643c8b4ec0c2a905601dcd3b49c1a28acb37e190
laerling <laerling@posteo.de>
2024-12-22 01:14:43 +0100

    phash.py: 1x1 to 3x3 (commented out) and 1x1 to 3x3 channel avg

--- phash.py
+++ phash.py
@@ -3,12 +3,15 @@
 import sys
 from PIL import Image, UnidentifiedImageError
 
-TO_DIM = 4
-
 if len(sys.argv) <= 1:
     print("Too few arguments. Expect image file")
     sys.exit(1)
 
+# We only have a "rough look" at the picture to see whether two pictures look
+# roughly the same. Therefore it's recommended to use this script in
+# conjunction with a conventional hash, because this script only shows very
+# rough differences, while a hash shows whether two pictures differ even in
+# just a small detail.
 for f in sys.argv[1:]:
 
     # load image, silently ignoring non-image files
@@ -16,32 +19,52 @@ for f in sys.argv[1:]:
         img = Image.open(f)
     except UnidentifiedImageError:
         continue
+    (width, height) = img.size
 
-    # TODO: Convert alpha to white instead of black, since that's how I want alpha to be displayed
+    # filter for resizing
+    # TODO: Which filter gives best result?
+    #       BOX, BILINEA, HAMMING, BICUBIC, LANCZOS
+    resampling_filter = Image.Resampling.LANCZOS
 
-    # TODO: Increase contrast to have a unified dynamic range (or whatever it's
-    #       called) across all images and to distinguish between different
-    #       details in images with low contrast
+    # TODO
+    # for dim in range(1,4+1):
+    #  for color in range(2):
+    #   for depth_p in range(1,4+1):
 
-    # convert to grayscale, since we're not interested in color shifts
-    img = img.convert('L')
-
-    # inner square (i. e. using n**2 of (n+2)**2 pixels) with n going from 1 to TO_DIM
-    for n in range(1, TO_DIM+1):
-
-        # resize
-        # TODO: Which filter gives best result?
-        #       BOX, BILINEA, HAMMING, BICUBIC, LANCZOS
-        resampling_filter = Image.Resampling.LANCZOS
-        img_ = img.resize((n+2,n+2), resampling_filter)
-
-        # FIXME make bit array and print as bytes (e. g. 4*256/16 would be 4 bytes,
-        # but 4*256/8 would be 5)
+    # PHASE 1: Overall color (1x1 pixel) and rough color distribution (2x2, 3x3, ...).
+    # TODO better to do subphases for 1) lightness/luminosity 2) mean color (over all channels) 3) mean color per channel
+    #      Means can probably be calculated in one iteration, without having to convert the image to grayscale first.
+    # TODO Calculate profile of normalized deviation from the black-white axis within the RGB cube?
+    max_dim = 3
+    printed = 0
+    for n in range(1, max_dim+1):
+        img_ = img.resize((n,n), resampling_filter)
         px = img_.load()
-        for i in range(1, n+1):
-            for j in range(1, n+1):
-                print(f"{px[i,j]//16:1x}", end='')
+        #print(f"n=={n}")
+        for i in range(0, n):
+            for j in range(0, n):
+                #print(f"{i} {j}:")
+                p_avg = 0
+                # FIXME treat alpha channel as letting through white, not black
+                # RGB channels
+                for c in range(3):
+                    #print(f"{px[i,j][c]//16:1x}", end='')  # 2 1x1 to 3x3
+                    # FIXME if the picture is grayscale px[i,j] is int
+                    #p_avg += px[i,j][c]
+                p_avg //= 3
+                print(f"{p_avg//16:1x}",end='')  # 3 1x1 to 3x3 channel avg
+                printed += 1
         print(end=' ')
 
+    # remaining amount of digits to reach a power of two
+    p = 1
+    truehash_len = 2**p
+    min_th_len = 8
+    while truehash_len < printed + min_th_len:
+        p += 1
+        truehash_len = 2**p
+    truehash_len -= printed
+    truehash_len //= 2
+
     # print filename
-    print(f" {f}")
+    print(f"  {f}")
